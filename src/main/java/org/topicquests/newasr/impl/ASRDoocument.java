5de44d8210d2f3afb1671854bb07b8cc4e167dae
Jack Park <jackpark@gmail.com>
2023-03-30 12:59:56 -0700

    Added licence block

--- src/main/java/org/topicquests/newasr/impl/ASRDoocument.java
+++ src/main/java/org/topicquests/newasr/impl/ASRDoocument.java
@@ -1,6 +1,9 @@
-/**
- * 
+/*
+ * Copyright 2023 TopicQuests Foundation
+ *  This source code is available under the terms of the Affero General Public License v3.
+ *  Please see LICENSE.txt for full license terms, including the availability of proprietary exceptions.
  */
+
 package org.topicquests.newasr.impl;
 
 import org.topicquests.newasr.api.IDocument;
--- src/main/java/org/topicquests/newasr/util/JsonUtil.java
+++ src/main/java/org/topicquests/newasr/util/JsonUtil.java
@@ -1,6 +1,9 @@
-/**
- * 
+/*
+ * Copyright 2023 TopicQuests Foundation
+ *  This source code is available under the terms of the Affero General Public License v3.
+ *  Please see LICENSE.txt for full license terms, including the availability of proprietary exceptions.
  */
+
 package org.topicquests.newasr.util;
 
 import com.google.gson.JsonObject;
