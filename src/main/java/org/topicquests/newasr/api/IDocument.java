d366d61aa6cdf286375bd841bcd0e61e45b0d033
Jack Park <jackpark@gmail.com>
2023-04-04 14:43:27 -0700

    Starting conversation as different type of document

--- src/main/java/org/topicquests/newasr/api/IDocument.java
+++ src/main/java/org/topicquests/newasr/api/IDocument.java
@@ -3,7 +3,6 @@
  *  This source code is available under the terms of the Affero General Public License v3.
  *  Please see LICENSE.txt for full license terms, including the availability of proprietary exceptions.
  */
-
 package org.topicquests.newasr.api;
 
 import com.google.gson.JsonArray;
