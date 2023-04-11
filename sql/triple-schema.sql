f47574c86732cf6904257fa2bb2c0d20eaef57ce
Jack Park <jackpark@gmail.com>
2023-04-11 11:45:32 -0700

    Moving some triple code here

--- sql/triple-schema.sql
+++ sql/triple-schema.sql
@@ -1,6 +1,6 @@
 SET ROLE tq_admin;
 
-CREATE SEQUENCE IF NOT EXISTS public.topic_id_seq
+CREATE SEQUENCE IF NOT EXISTS public.triple_id_seq
     AS bigint
     START WITH 1
     INCREMENT BY 1
@@ -10,15 +10,16 @@ CREATE SEQUENCE IF NOT EXISTS public.topic_id_seq
 
 
 CREATE TABLE IF NOT EXISTS public.triple (
-	id 		BIGINT PRIMARY KEY NOT NULL DEFAULT nextval('public.topic_id_seq'::regclass),
+	id 		BIGINT PRIMARY KEY NOT NULL DEFAULT nextval('public.triple_id_seq'::regclass),
 	subj_id	BIGINT NOT NULL,
 	pred_id	BIGINT NOT NULL,
 	obj_id	BIGINT NOT NULL,
 	subj_typ 	TEXT NOT NULL,
-	obj_typ		TEXT NOT NULL
+	obj_typ		TEXT NOT NULL,
+	psi		TEXT	
 );
 
-CREATE INDEX IF NOT EXISTS triple_idx ON public.triple (id, subj_id, obj_id, subj_typ, obj_typ);
+CREATE INDEX IF NOT EXISTS triple_idx ON public.triple (id, subj_id, obj_id, subj_typ, obj_typ, psi);
 
 CREATE TABLE IF NOT EXiSTS public.sentenceids  (
 	id 			BIGINT PRIMARY KEY NOT NULL REFERENCES  public.triple (id),
