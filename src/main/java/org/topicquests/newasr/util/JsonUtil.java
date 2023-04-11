f9450c362dcbb756b9f387037b0651d9d0152a8f
Jack Park <jackpark@gmail.com>
2023-04-11 14:54:07 -0700

    Added new parser

--- src/main/java/org/topicquests/newasr/util/JsonUtil.java
+++ src/main/java/org/topicquests/newasr/util/JsonUtil.java
@@ -9,6 +9,9 @@ package org.topicquests.newasr.util;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+
 /**
  * @author jackpark
  *
@@ -29,6 +32,17 @@ public class JsonUtil {
 	 */
 	public JsonObject parse(final String jsonString) throws Exception {
 		return (JsonObject)JsonParser.parseString(jsonString);
+	}
+
+	/**
+	 * Parsing for old style JSONObject
+	 * @param json
+	 * @return
+	 * @throws Exception
+	 */
+	public JSONObject parseJsonObject(String json)  throws Exception {
+		JSONParser p = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
+		return(JSONObject)p.parse(json);
 	}
 
 }
