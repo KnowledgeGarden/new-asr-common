e5c0fd6b364bea8f4c10d134e38ccefd3ded2ee6
Jack Park <jackpark@gmail.com>
2023-04-11 14:16:11 -0700

    Removed fat jar compiling, updated schema

--- sql/triple-schema.sql
+++ sql/triple-schema.sql
@@ -16,7 +16,8 @@ CREATE TABLE IF NOT EXISTS public.triple (
 	obj_id	BIGINT NOT NULL,
 	subj_typ 	TEXT NOT NULL,
 	obj_typ		TEXT NOT NULL,
-	psi		TEXT	
+	psi		TEXT,
+	norm_id	BIGINT DEFAULT -1
 );
 
 CREATE INDEX IF NOT EXISTS triple_idx ON public.triple (id, subj_id, obj_id, subj_typ, obj_typ, psi);
@@ -25,3 +26,18 @@ CREATE TABLE IF NOT EXiSTS public.sentenceids  (
 	id 			BIGINT PRIMARY KEY NOT NULL REFERENCES  public.triple (id),
 	sentence_id	BIGINT NOT NULL
 );
+
+-- Working Triples are those which are not normalized
+-- they do not get sentence links
+CREATE TABLE IF NOT EXISTS public.working_triple (
+	id 		BIGINT PRIMARY KEY NOT NULL DEFAULT nextval('public.triple_id_seq'::regclass),
+	subj_id	BIGINT NOT NULL,
+	pred_id	BIGINT NOT NULL,
+	obj_id	BIGINT NOT NULL,
+	subj_typ 	TEXT NOT NULL,
+	obj_typ		TEXT NOT NULL,
+	psi		TEXT,
+	norm_id	BIGINT DEFAULT -1
+);
+
+CREATE INDEX IF NOT EXISTS wtriple_idx ON public.working_triple (id, subj_id, obj_id, subj_typ, obj_typ, psi);
